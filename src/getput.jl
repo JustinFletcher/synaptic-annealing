eed570086f2406ab245c02c7a75a35797275d4d2
Justin Fletcher <justinr.fletcher@gmail.com>
2015-10-24 20:34:03 -0400

    Restructured dirs to comply with best practices.

--- src/getput.jl
+++ src/getput.jl
@@ -1,13 +1,13 @@
 function putdata(data, idStr)
     # This function is a convienience interface to the serialize package.
-    out = open("data\\"*idStr,"w")
+    out = open(homedir()*"\\OneDrive\\afit\\rs\\synaptic-annealing\\cache\\"*idStr,"w")
     serialize(out,data);
     close(out)
 end
 
 function getdata(idStr)
     # This function is a convienience interface to the serialize package.
-    in = open("data\\"*idStr,"r")
+    in = open(homedir()*"\\OneDrive\\afit\\rs\\synaptic-annealing\\cache\\"*idStr,"r")
     data = deserialize(in)
     close(in)
     return(data)
